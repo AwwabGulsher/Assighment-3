ff918fac8d10782c911bdc786193c29f755941d5
Awwabgulsher <Awwabgulsher6@gmail.com>
2025-11-19 14:57:49 -0800

    Done

---
+++ script.js
@@ -0,0 +1,70 @@
+console.log("Assighment#3")
+
+// Q1:
+// Ans:1
+// let a = 3
+// let b= 5
+// sum= 3+5
+// console.log(sum)
+
+// Q2:
+// Ans:2
+// let a = 3
+// let b= 5
+// sum= 3+5
+// console.log(sum)
+
+// Ans:2
+// let a = 3
+// let b= 5
+// sum= 3-5
+// console.log(sum)
+
+// Ans:2 
+// let a = 3
+// let b= 5
+// sum= 3*5
+// console.log(sum)
+
+// Ans:2 
+// let a = 3
+// let b= 5
+// sum= 3/5
+// console.log(sum)
+
+// Ans:2 
+// let a = 3
+// let b= 5
+// sum= 3%5
+// console.log(sum)
+
+// Q3:
+// Ans:3
+
+
+
+
+
+
+
+
+
+// Q4:
+// Ans:4
+// let TicketMovie = 600;
+// let byingtiket= 5
+// sum= 600*5
+// console.log("Total cost to buy 5 tikets to a movie is",sum )
+Q5:
+Ans:5
+// let a = prompt("enter your number,")
+// document.write(a, " X 1 =", a * 1, "<br>")
+// document.write(a, " X 2 =", a * 2, "<br>")
+// document.write(a, " X 3 =", a * 3, "<br>")
+// document.write(a, " X 4 =", a * 4, "<br>")
+// document.write(a, " X 5 =", a * 5, "<br>")
+// document.write(a, " X 6 =", a * 6, "<br>")
+// document.write(a, " X 7 =", a * 7, "<br>")
+// document.write(a, " X 8 =", a * 8, "<br>")
+// document.write(a, " X 9 =", a * 9, "<br>")
+// document.write(a, " X 10 =", a * 10, "<br>")
